7a8657a2f7328b3f98075fbcfd36f0c03c51a003
Faruk Furkan Özduran <ffurkan1999@gmail.com>
2026-07-06 01:07:31 +0300

    Add project files.

---
+++ Project.AtYarisi.API/Models/Jockey.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.AtYarisi.API.Models
+{
+    public class Jockey
+    {
+        [Key]
+        public int JockeyId { get; set; }
+        public string JockeyName { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Weight { get; set; }
+        public int Experience { get; set; }
+        public bool IsLicenseActive { get; set; }
+    }
+}
---
+++ Project.AtYarisi.API/Models/Sponsor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.AtYarisi.API.Models
+{
+    public class Sponsor
+    {
+        [Key]
+        public int SponsorId { get; set; }
+        public string BrandName { get; set; }
+        public string Sector { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal AnnualBudget { get; set; }
+        public string Email { get; set; }
+    }
+}
